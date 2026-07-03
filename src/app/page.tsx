52d09c2ccd58f472bb466a24c3e509e9b154e13a
Liz <miextr4@gmail.com>
2026-07-03 11:41:11 +0100

    Add CRUD functionality - Create, Read, Update, Delete applications with Supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,57 @@
-export default function Home() {
+import supabaseConnection from '@/lib/supabase';
+import Link from 'next/link';
+import DeleteButton from '@/components/DeleteButton';
+
+export default async function Home() {
+
+const { data } = await supabaseConnection.from('applications').select('*').order('created_at', {ascending: false});
+
   return (
     <main>
       <h1 className="text-3xl font-bold">Job Application Tracker</h1>
+      <Link href="/applications/new">Add New Application</Link>
+      <table>
+        <thead>
+            <tr>
+              <th>Job Title</th>
+              <th>Company</th>
+              <th>Salary</th>
+              <th>Status</th>
+              <th>Date Applied</th>
+              <th>Notes</th>
+              <th>Feedback</th>
+              <th>Job URL</th>
+              <th>Actions</th>
+            </tr>
+        </thead>
+        {data?.length > 0 ? (<tbody>
+          {data?.map((app) => {
+            return (
+              <tr key={app.id}>
+                <td>{app.job_title}</td>
+                <td>{app.company}</td>
+                <td>{app.salary}</td>
+                <td>{app.status}</td>
+                <td>{app.date_applied}</td>
+                <td>{app.notes}</td>
+                <td>{app.feedback}</td>
+                <td>{app.job_url}</td>
+                <td>
+                  <Link href={`/applications/${app.id}/`}>Edit</Link>
+                  <DeleteButton id={app.id}/>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>) : 
+        (<tbody>
+          <tr>
+            <td colSpan={9}>
+              No applications added yet
+            </td>
+          </tr>
+        </tbody>)}
+      </table>
     </main>
   );
 }
